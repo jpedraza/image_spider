62cffab02cc68a3df8de5657bb2c6e14e92a06c8
lufo <lufo816@gmail.com>
2015-08-07 19:17:21 +0800

    bing

--- bing_search_api.py
+++ bing_search_api.py
@@ -4,18 +4,11 @@ __author__ = 'lufo'
 import requests  # Get from https://github.com/kennethreitz/requests
 import string
 import os
+import urllib
 import time
 from multiprocessing.dummy import Pool as ThreadPool
 
 KEY = "kGHwFzi2I1zI8dA8GiRRHtGsrW8qqUgLXLKUatCUpn8"
-
-header = {
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36",
-    'Host': "www.xingyun.cn",
-    'Referer': "http://www.xingyun.cn/",
-    'X-Requested-With': "XMLHttpRequest"
-}
-
 
 class BingSearchAPI():
     bing_api = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Composite?"
@@ -96,15 +89,15 @@ def save_img(img_url_list, path):
     :param img_url: 图片url list
     :param path: 保存的路径
     """
-    global header
     for i, img_url in enumerate(img_url_list):
         with open(path + str(i) + '.jpg', 'wb') as fw:
             try:
-                fw.write(requests.get(img_url, timeout=5, headers=header, allow_redirects=False).content)
+                # fw.write(urllib.urlopen(img_url).read())
+                # urllib.urlretrieve(img_url, path + str(i) + '.jpg')
+                fw.write(requests.get(img_url, timeout=5, allow_redirects=False).content)
             except Exception, e:
                 print img_url
                 print e
-            time.sleep(0.1)
 
 
 def save_all_img(file_list):
@@ -165,6 +158,7 @@ def get_file_num(path='./europe_orig_sorted/'):
 
 
 if __name__ == "__main__":
+    # save_all_img(file_list)
     # save_img_url_main()
     save_img_main(number_of_threads=4)
     # get_file_num()
